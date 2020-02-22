5405ef3ddb9996d8c76bc42af62564e7e8547cf6
wadrennan <wadrennan@crimson.ua.edu>
2020-02-22 02:03:59 -0600

    Hooking in to API
    Implemented API request on create. Just prints JSON response to screen as of now.

--- app/src/main/java/com/example/trendingtrails/WeatherActivity.java
+++ app/src/main/java/com/example/trendingtrails/WeatherActivity.java
@@ -1,19 +1,25 @@
 package com.example.trendingtrails;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONObject;
+
+
 public class WeatherActivity extends AppCompatActivity {
-    private RequestQueue queue;
-    private TextView myTextView;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    //private RequestQueue queue;
+    //private TextView myTextView;
+    //@Override
+    /*protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
         final TextView myTextView = (TextView) findViewById(R.id.weatherData);
@@ -26,7 +32,33 @@ public class WeatherActivity extends AppCompatActivity {
                 myTextView.setText("hello");
                 }
             });
-        }
+        }*/
+    private TextView txtDisplay;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+        txtDisplay = (TextView) findViewById(R.id.weatherData);
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://api.weather.gov/points/39.7456,-97.0892";
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+// TODO Auto-generated method stub
+                txtDisplay.setText("Response => " + response.toString());
+              //  findViewById(R.id.progressBar1).setVisibility(View.GONE);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                txtDisplay.setText("That didn't work!");
+            }
+        });
+        queue.add(jsObjRequest);
+
+    }
+
 }
 
 
