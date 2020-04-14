a1823b9d1a6cc78a9ba69723f8e5f4dbabe63b6a
Cole Morgan <cmorgan@deroyal.com>
2020-04-14 15:54:57 -0400

    Need to pass id not the trail object

--- app/src/main/java/com/example/trendingtrails/Profile/AddedTrailsFragment.java
+++ app/src/main/java/com/example/trendingtrails/Profile/AddedTrailsFragment.java
@@ -98,7 +98,7 @@ public class AddedTrailsFragment extends Fragment {
                     @Override
                     public void onClick(View v) {
                         Intent intent = new Intent(getContext(), TrailInfoActivity.class);
-                        intent.putExtra("Trail", trail);
+                        intent.putExtra("Trail", trail.id);
                         startActivity(intent);
                     }
                 });
