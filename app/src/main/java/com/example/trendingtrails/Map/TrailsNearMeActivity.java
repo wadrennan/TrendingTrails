084df7bd115abf4f2f63dead47c51827f5037cc9
wadrennan <wadrennan@crimson.ua.edu>
2020-04-14 13:41:58 -0600

    Added distance select on trails near me
    -- Selectable distance on trails near me
    --Truncated distance values in popular trails and trails near me

--- app/src/main/java/com/example/trendingtrails/Map/TrailsNearMeActivity.java
+++ app/src/main/java/com/example/trendingtrails/Map/TrailsNearMeActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,6 +20,8 @@ import com.example.trendingtrails.Models.Trail;
 import com.example.trendingtrails.R;
 import com.google.android.gms.maps.model.LatLng;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -29,6 +34,10 @@ public class TrailsNearMeActivity extends BaseActivity implements TrailsViewAdap
     private RecyclerView trailCards;
     private double lat;
     private double lon;
+    private Spinner selectDistance;
+    private String currentSelected;
+    private double lonRad;
+    private double latRad;
 
 
     LocationTrack lt;
@@ -45,8 +54,8 @@ public class TrailsNearMeActivity extends BaseActivity implements TrailsViewAdap
             lon = lt.getLongitude();
         }
         System.out.println(lat+" "+lon);
-        double latRad = deg2rad(lat);
-        double lonRad = deg2rad(lon);
+        latRad = deg2rad(lat);
+        lonRad = deg2rad(lon);
         trailCards = (RecyclerView) findViewById(R.id.recycler_view);
         conn = Database.connect();
         if(conn == null){
@@ -59,6 +68,7 @@ public class TrailsNearMeActivity extends BaseActivity implements TrailsViewAdap
             System.out.println(query);
             new TrailsNearMeActivity.QueryDb().execute(query);
         }
+        initSpinner();
     }
 
     @Override
@@ -74,7 +84,6 @@ public class TrailsNearMeActivity extends BaseActivity implements TrailsViewAdap
         //System.out.println("Trail "+name+" clicked! id = "+id);
 
     }
-   //TODO: Implement function to navigate to map existing trail
 
     private class QueryDb extends AsyncTask<String,Void, List<Trail>> {
         //Queries db and populates a list of results asynchronously
@@ -92,6 +101,9 @@ public class TrailsNearMeActivity extends BaseActivity implements TrailsViewAdap
                     id = rs.getInt("trail_id");
                     name =  rs.getString("name");
                     dist = rs.getDouble("distance");
+                    BigDecimal truncatedDist = new BigDecimal(Double.toString(dist));
+                    truncatedDist = truncatedDist.setScale(2, RoundingMode.HALF_UP);
+                    dist = truncatedDist.doubleValue();
                     Trail t = new Trail(id, name,0,0,dist);
                     trailList.add(t);
                     rs.next();
@@ -127,5 +139,67 @@ public class TrailsNearMeActivity extends BaseActivity implements TrailsViewAdap
     private void sqlError(){
         Toast.makeText(getApplicationContext(), "Trails Near Me Unavailable", Toast.LENGTH_LONG).show();
     }
+
+    private void initSpinner(){
+        List<String> distances = new ArrayList<String>();
+        distances.add("25 miles");
+        distances.add("50 miles");
+        distances.add("100 miles");
+        distances.add("100 miles +");
+        selectDistance = findViewById(R.id.distance_spinner);
+        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, distances);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        selectDistance.setAdapter(spinnerAdapter);
+        selectDistance.setSelection(1);
+        currentSelected = "50 miles";
+
+        //Listener
+        selectDistance.setOnItemSelectedListener(
+                new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                        if(selectDistance.getSelectedItem().toString() == currentSelected){
+                            // do nothing
+                            System.out.println("This item was already selected");
+                        }
+                        else{
+                            String op;
+                            currentSelected = selectDistance.getSelectedItem().toString();
+                            double dist;
+                            if(currentSelected == "25 miles"){
+                                dist = 40.2336;
+                                op = "<=";
+                            }
+                            else if(currentSelected == "50 miles"){
+                                dist = 80.4672;
+                                op = "<=";
+                            }
+                            else if(currentSelected == "100 miles"){
+                                dist = 160.934;
+                                op = "<=";
+                            }
+                            else{
+                                dist = 160.934;
+                                op = ">=";
+                            }
+
+                            trailCards.removeAllViews();
+                            trailCards.setAdapter(null);
+                            String query = "SELECT trail_id, name, distance FROM AllTrails where acos(sin("+latRad+") * sin(RADIANS(lat)) + cos("+latRad+") * cos(RADIANS(lat)) * cos(RADIANS(long) - ("+lonRad+"))) * 6371 "+op+" "+dist+"";
+                            System.out.println(query);
+                            new TrailsNearMeActivity.QueryDb().execute(query);
+
+                        }
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> parent) {
+                        System.out.println("Nothing Selected");
+                    }
+                }
+        );
+    }
+
+
 
 }
